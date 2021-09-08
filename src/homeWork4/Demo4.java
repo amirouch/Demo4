c78fbeb3216da777715c6e145650f02a216d85a3
amirouch <tech.tabti@gmail.com>
2021-09-08 07:54:11 -0400

    Update Demo4.java

--- src/homeWork4/Demo4.java
+++ src/homeWork4/Demo4.java
@@ -5,6 +5,8 @@ public class Demo4 {
 	public static void main(String[]args) {
 		
 		System.out.println("home work number 4");
+		System.out.println("home work number 40");
+		System.out.println("home work number 400");
 		
 	}
 
