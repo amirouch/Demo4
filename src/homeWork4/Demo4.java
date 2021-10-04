359b68a15f4a974b501583640d1511f06edad853
amirouch <tech.tabti@gmail.com>
2021-10-04 19:03:14 -0400

    1st commit

--- src/homeWork4/Demo4.java
+++ src/homeWork4/Demo4.java
@@ -1,5 +1,7 @@
 package homeWork4;
 
+import java.util.Scanner;
+
 public class Demo4 {
 	
 	public static void main(String[]args) {
@@ -7,6 +9,15 @@ public class Demo4 {
 		System.out.println("home work number 4");
 		System.out.println("home work number 40");
 		System.out.println("home work number 400");
+		
+		int a=5;
+		
+		Scanner obj =new  Scanner(System.in);
+		
+		System.out.println("plz enter a number");
+		String b = obj.next();
+		
+		System.out.println( b +5);
 		
 	}
 
